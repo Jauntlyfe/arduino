8d6ebe399171f5f2133ad3655a86c93be1fcf47b
Jauntlyfe <52168168+Jauntlyfe@users.noreply.github.com>
2019-10-09 16:47:27 -0400

    Update Keypad.c

--- Keypad.c
+++ Keypad.c
@@ -1,12 +1,5 @@
 #include <Keypad.h>
 
-/* sketch 3 
-turn on a LED when the button is pressed and let it on 
-until the button is pressed again
-*/
-const int OnPinButton = 2;
-const int OffPinButton = 4;
-const int ledPin = 13;
 const byte ROWS = 4;
 const byte COLS = 4;
 char hexaKeys[ROWS][COLS] = {
@@ -20,8 +13,6 @@ byte rowPins[ROWS] = {12, 11, 10, 9};
 byte colPins[COLS] = {8, 7, 6, 5};
 
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
-
-
 
 void setup() {
   pinMode(OnPinButton, INPUT);
@@ -37,21 +28,7 @@ void loop() {
   if (customKey){
     Serial.println(customKey);
   }
-  //Serial.print("Success");
- int checkOnButton = digitalRead(OnPinButton);  
- int checkOffButton = digitalRead(OffPinButton);
 
-    if(checkOnButton == HIGH){
-   
-    digitalWrite(ledPin, LOW);
-  }
-  
-
-  if(checkOffButton == HIGH){
-   
-    digitalWrite(ledPin, HIGH);
-    
-  }
 
   
 }
